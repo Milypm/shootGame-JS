133c736a06fb78b0c1415903ee971364e36ad99e
Mily <mp_morones@hotmail.com>
2021-06-03 10:14:18 -0500

    Install Jest, manage dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +7,11 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'typeof CANVAS_RENDERER': JSON.stringify(false),
+      'typeof WEBGL_RENDERER': JSON.stringify(true)
+    })
+  ]
 };
