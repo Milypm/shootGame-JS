4b15652d8f99510841a66c323a6f4a8658d951bc
Mily <mp_morones@hotmail.com>
2021-06-16 13:06:25 -0500

    Remove previous dist directory

--- webpack/prod.js
+++ webpack/prod.js
@@ -7,7 +7,6 @@ module.exports = merge(base, {
   mode: "production",
   output: {
     filename: "bundle.min.js",
-    path: path.resolve(__dirname, 'dist'),
   },
   devtool: false,
   performance: {
