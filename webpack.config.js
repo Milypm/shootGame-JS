f50838806cbe2394afb96c940ef37af9fb187d1f
Mily <mp_morones@hotmail.com>
2021-06-17 16:26:11 -0500

    Remove mode:none from webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  mode: 'none',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
